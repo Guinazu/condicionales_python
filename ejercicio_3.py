f55da2f255546ddfe001dfe3bdf46b8a901a67b7
Guinazu <111105873+Guinazu@users.noreply.github.com>
2022-09-01 16:19:27 -0300

    Add files via upload
    Hola, ejercicios resueltos.

---
+++ ejercicio_3.py
@@ -0,0 +1,32 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejemplos variables de texto
+
+# Ingrese primero su nombre y luego su apellido
+# Capture ambos datos e imprima su nombre completo
+print('Ingrese por consola su nombre/s:')
+nombre = str(input())
+
+print('Ingrese por consola su apellido/s:')
+apellido = str(input())
+
+# Imprima su nombre completo
+
+# Almacenar su nombre completo en una variable
+# nombre_completo = .....
+
+# Imprimir la cantidad de letras que posee su nombre completo
+# cantidad_letras = len(....)
+
+print("mi nombre completo es", nombre, apellido)
+nombre_completo = 'hernan' 'guiñazu' 
+hernan_guiñazu_len = len(nombre_completo)
+print("mi nombre tiene, len('nombre'_'completo', 'letras'")
+
